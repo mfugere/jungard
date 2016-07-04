8da6fed305f47469339a4b558e72ad89bc8a5a1e
mfugere <mjfugere@gmail.com>
2016-07-04 17:04:54 -0400

    Separate files for helper functions and data

---
+++ js/helpers.js
@@ -0,0 +1,21 @@
+var diceRoll = function (sides, numDice) {
+    if (numDice === undefined) numDice = 1;
+    var total = 0;
+    for (var i = 0; i < numDice; i++) {
+        total += Math.floor(Math.random() * sides) + 1;
+    }
+    return total;
+}
+
+var getMemberByRef = function (array, ref) {
+    for (var i in array) {
+        if (array[i].ref === ref) return array[i];
+    }
+};
+
+var interpolate = function (string, values) {
+    for (var i = 0; i < values.length; i++) {
+        string = string.replace("{" + i + "}", values[i]);
+    }
+    return string;
+};
